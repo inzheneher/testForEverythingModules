0f4be68f6305da0f0e117a5b1113aae5f851a27e
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2020-01-14 16:31:32 -0500

    add submit form to specify search path

--- fileCounter/src/main/java/gov/controller/FileCounterController.java
+++ fileCounter/src/main/java/gov/controller/FileCounterController.java
@@ -1,21 +1,39 @@
 package gov.controller;
 
+import gov.dao.PathCollector;
 import gov.service.FileScanner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Controller
 public class FileCounterController {
 
+    private Logger logger = Logger.getLogger(FileCounterController.class.getName());
+
     @GetMapping("/find_movies")
     public String findMovies(Model model) {
-        List<String> list = new FileScanner().scanner(new String[] {"/some/path/", "/home/inzheneher/"});
+        List<String> list = new FileScanner().scanner(new String[]{"/some/path/", "/home/inzheneher/"});
         model.addAttribute("list", list);
         return "find_movies";
+    }
+
+    @GetMapping("/find")
+    public String showSubmitForm(Model model) {
+        model.addAttribute("paths", new PathCollector());
+        return "input_path";
+    }
+
+    @PostMapping("/input_path")
+    public String submitPathsAndFindMovies(@ModelAttribute PathCollector paths, Model model) {
+        logger.info("Paths: " + paths.getPath());
+        List<String> list = new FileScanner().scanner(paths.getPath().split(" "));
+        model.addAttribute("list", list);
+        return "results";
     }
 }
---
+++ fileCounter/src/main/java/gov/dao/PathCollector.java
@@ -0,0 +1,14 @@
+package gov.dao;
+
+public class PathCollector {
+
+    private String path;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
