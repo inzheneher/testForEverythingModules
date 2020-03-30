c088f6dd9362ab0f53ffb8a0fe1bb7c5bddc7526
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2020-03-30 18:44:30 -0400

    Added finding anagram consist of one character.

--- streams/src/main/java/strings/StringToArray.java
+++ streams/src/main/java/strings/StringToArray.java
@@ -1,8 +1,5 @@
 package strings;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class StringToArray {
 
     public static void main(String[] args) {
@@ -10,20 +7,10 @@ public class StringToArray {
         int counter = 0;
 
         String string = "ifailuhkqq"; //ifailuhkqq = 3; abcdaba = ?; kkkk = 10;
-        List<Character> characters = string
-                .chars()
-                .mapToObj(c -> (char) c)
-                .sorted(Character::compareTo)
-                .collect(Collectors.toList());
-        characters
-                .stream()
-                .map(l -> l + " ")
-                .forEach(System.out::print);
-        System.out.println();
 
-        for (int i = 0; i < characters.size() - 1; i++) {
-            for (int j = i + 1; j < characters.size(); j++) {
-                if (characters.get(i) == characters.get(j)) {
+        for (int i = 0; i < string.length() - 1; i++) {
+            for (int j = i + 1; j < string.length(); j++) {
+                if (string.substring(i, i + 1).equals(string.substring(j, j + 1))) {
                     counter++;
                 }
             }
