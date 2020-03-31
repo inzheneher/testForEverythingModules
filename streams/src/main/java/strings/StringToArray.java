dece5c2b85b99588e619c94c7ea2892e627d5b18
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2020-03-31 11:44:16 -0400

    Added anagram decision with simple arrays.

--- streams/src/main/java/strings/StringToArray.java
+++ streams/src/main/java/strings/StringToArray.java
@@ -1,21 +1,42 @@
 package strings;
 
+import java.util.Arrays;
+
 public class StringToArray {
 
     public static void main(String[] args) {
 
         int counter = 0;
 
-        String string = "ifailuhkqq"; //ifailuhkqq = 3; abcdaba = ?; kkkk = 10;
+        String string = "abcdaba"; //ifailuhkqq = 3; abcdaba = ?; kkkk = 10; cdcd = 5;
 
-        for (int i = 0; i < string.length() - 1; i++) {
-            for (int j = i + 1; j < string.length(); j++) {
-                if (string.substring(i, i + 1).equals(string.substring(j, j + 1))) {
-                    counter++;
+        try {
+
+            for (int k = 0; k < string.length() - 1; k++) {
+                for (int i = 0; i < string.length() - 1; i++) {
+                    for (int j = i + 1; j < string.length() - k; j++) {
+                        String substring1 = string.substring(i, i + 1 + k);
+                        String substring2 = string.substring(j, j + 1 + k);
+
+                        char[] subStrChar1 = substring1.toCharArray();
+                        char[] subStrChar2 = substring2.toCharArray();
+
+                        Arrays.sort(subStrChar1);
+                        Arrays.sort(subStrChar2);
+
+                        if (Arrays.equals(subStrChar1, subStrChar2)) {
+                            System.out.println(subStrChar1);
+                            System.out.println(subStrChar2);
+                            counter++;
+                        }
+                    }
                 }
             }
-        }
 
-        System.out.println("Number of similar elements in array is: ".concat(String.valueOf(counter)));
+            System.out.println("Number of similar elements in array is: ".concat(String.valueOf(counter)));
+
+        } catch (StringIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
     }
 }
