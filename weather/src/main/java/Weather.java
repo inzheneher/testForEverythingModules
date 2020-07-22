3f1fd5fce6de1d9ec3f8ca7db3a2eca85340cb67
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2020-07-22 12:45:04 -0400

    Added properties to gitignore. Created OpenWeatherResponse class from json. Added to Weather class reading properties from file.

--- weather/src/main/java/Weather.java
+++ weather/src/main/java/Weather.java
@@ -1,11 +1,13 @@
 import com.google.gson.Gson;
-import dao.WeatherResponse;
+import responses.OpenWeatherResponse;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Properties;
 
 /**
  * inzheneher created on 15/07/2020 inside the package - PACKAGE_NAME WeatherResponse
@@ -15,7 +17,18 @@ public class Weather {
 
     public static void main(String[] args) {
         try {
-            URL url = new URL("https://api.weather.gov/gridpoints/BOX/21,35/forecast");
+            Properties prop = new Properties();
+            //TODO: use relative path
+            prop.load(new FileInputStream("/home/inzheneher/WORK/TEST/testForEverythingModules/weather/src/main/resources/request.properties"));
+            //TODO: read API from file
+            //TODO: get coordinates from web page
+            String baseUrl = prop.getProperty("baseUrl");
+            String lat = "-19.134422";
+            String lon = "131.638687";
+            String units = prop.getProperty("units");
+            String appId = prop.getProperty("appId");
+            String requestUrl = String.format("%slat=%s&lon=%s&units=%s&appid=%s", baseUrl, lat, lon, units, appId);
+            URL url = new URL(requestUrl);
             HttpsURLConnection httpsConn = (HttpsURLConnection) url.openConnection();
             httpsConn.setRequestMethod("GET");
             httpsConn.setRequestProperty("Content-Type", "application/json");
@@ -24,13 +37,11 @@ public class Weather {
             StringBuilder response = new StringBuilder();
             BufferedReader in = new BufferedReader(new InputStreamReader(httpsConn.getInputStream()));
             String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
+            while ((inputLine = in.readLine()) != null) response.append(inputLine);
             in.close();
-            WeatherResponse weatherResponse = new Gson().fromJson(response.toString(), WeatherResponse.class);
-            weatherResponse.getProperties().getPeriods().forEach(a ->
-                    System.out.printf("Temperature on %s is: %s\n", a.getName(), a.getTemperature()));
+            OpenWeatherResponse weatherResponse = new Gson().fromJson(response.toString(), OpenWeatherResponse.class);
+            System.out.printf("Temperature in %s is: %s\u00B0C", weatherResponse.getSys().getCountry(), weatherResponse.getMain().getTemp());
+            ;
         } catch (IOException e) {
             e.printStackTrace();
         }
