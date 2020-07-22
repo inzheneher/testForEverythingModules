368364c0a900dbf44085a84dc2938cdacfacde81
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2020-07-22 14:22:56 -0400

    Added spring dependencies to module pom. Added 2 controllers for start page and for result page.

---
+++ weather/src/main/java/application/Application.java
@@ -0,0 +1,14 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * inzheneher created on 22/07/2020 inside the package - application
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ weather/src/main/java/controllers/WeatherController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * inzheneher created on 22/07/2020 inside the package - controllers
+ */
+@Controller
+public class WeatherController {
+
+}
