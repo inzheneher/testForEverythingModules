478bc690f02f7d0e66909fc59f311bfb6cee0414
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2021-01-26 16:28:52 -0500

    Change approach to identify IP address  Changes to be committed: 	modified:   regex/src/main/java/Main.java

--- regex/src/main/java/Main.java
+++ regex/src/main/java/Main.java
@@ -1,4 +1,3 @@
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -7,12 +6,17 @@ import java.util.regex.Pattern;
 public class Main {
     private final static String IP4V_REGEX = "(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
     private final static String IP6V_REGEX = "(([0-9a-fA-F]{1,4}):){7}([0-9a-fA-F]{1,4})";
+
     public static void main(String[] args) {
+        System.out.printf("First input is: %s%n", validIPAddress("192.168.255.1"));
+        System.out.printf("Second input is: %s", validIPAddress("2001:0db8:85a3:0:0:8A2E:0370:7334"));
+    }
+
+    public static String validIPAddress(String IP) {
         Pattern patIP4 = Pattern.compile(IP4V_REGEX);
         Pattern patIP6 = Pattern.compile(IP6V_REGEX);
-        Matcher matIP4 = patIP4.matcher("192.168.255.1");
-        Matcher matIP6 = patIP6.matcher("2001:0db8:85a3:0:0:8A2E:0370:7334");
-        System.out.printf("First input is: %s%n", matIP4.matches()? "IPv4" : "Neither");
-        System.out.printf("Second input is: %s", matIP6.matches()? "IPv6" : "Neither");
+        if (patIP4.matcher(IP).matches()) return "IPv4";
+        else if (patIP6.matcher(IP).matches()) return "IPv6";
+        else return "Neither";
     }
 }
